f297458d4faef5fa3088c36fc0958613764d95dd
upr8 <134240395+upr8@users.noreply.github.com>
2025-12-28 22:49:10 +1100

    build: add commitlint with husky for commit message validation

---
+++ commitlint.config.js
@@ -0,0 +1,34 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+    'scope-enum': [
+      1,
+      'always',
+      ['core', 'ios', 'android', 'web', 'docs', 'deps'],
+    ],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'header-max-length': [2, 'always', 72],
+    'body-max-line-length': [2, 'always', 100],
+  },
+};
